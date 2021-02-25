0e89056614413291ead2246e69bbfba0af18af23
kzl <laszlo.zoltan.kiss@gmail.com>
2021-02-25 12:28:48 +0100

    .

---
+++ apache-ecs/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wp_admin');
+define('DB_PASSWORD', '');
+define('DB_HOST', '');
+define('DB_COLLATE', 'utf8_general_ci');
+define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
+?>
