c2e40ab6485473fa0f371c50e85320e560059989
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-03-30 17:48:03 +0200

    .

--- mysql/install2.sh
+++ mysql/install2.sh
@@ -8,8 +8,31 @@
 # Jenkins variables:
 # MYSQL_ROOT as secret text
 
-apt update
-apt install mysql-server -y
+# Update system
+yum update -y 
+
+# Install required tools
+yum install wget -y
+
+# Download MySQL 5.7
+wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+
+# Install MySQL 5.7
+rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+yum install mysql-server -y 
+
+# Start MySQL service (enabled by default):
+systemctl start mysqld
+
+# Headless secure install: (not working)
+# Source: https://gist.github.com/vdvm/24754bf1aee6fd85e1aa
+
+# As non root user:
+cat <<EOF >> ~/.my.cnf
+[client]
+user = root
+EOF
+echo "password = $(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $11}')" >> ~/.my.cnf 
 
 mysql_secure_installation
 
