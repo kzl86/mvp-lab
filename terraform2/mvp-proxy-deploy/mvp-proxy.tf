ae3a3ff91ed7226f7ee4085f91dffc734b242e90
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-03-30 13:03:43 +0200

    .

--- terraform2/mvp-proxy-deploy/mvp-proxy.tf
+++ terraform2/mvp-proxy-deploy/mvp-proxy.tf
@@ -4,17 +4,34 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-variable "aws_access_key" {
-  type    = string
-}
+variable "aws_access_key" { type = string }
+variable "aws_secret_key" { type = string }
+variable "subnet-id"      { type = string }
+variable "vpc-id"         { type = string } 
 
-variable "aws_secret_key" {
-  type    = string
-}
+resource "aws_security_group" "proxy" {
+  name        = "proxy"
+  description = "Allow SSH for admin"
+  vpc_id      = var.vpc-id
 
-variable "subnet-id" {
-  type    = string
-  default = "start123"
+  ingress {
+    description = "SSH inbound"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "mvp-proxy"
+  }
 }
 
 resource "aws_instance" "mvp-proxy" {
@@ -23,7 +40,8 @@ resource "aws_instance" "mvp-proxy" {
   key_name               = "zoltan.kiss_training_terraform"
   subnet_id              = var.subnet-id
   user_data              = file("../prepareJenkinsNode.sh")
-  
+  vpc_security_group_ids = [ aws_security_group.proxy.id ]
+
   tags = {
     Name = "mvp-proxy"
   }
