dda64b09c2a1f76e53ce50a362939a55afadd3f6
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-04-06 12:57:37 +0200

    .

--- terraform2/mvp-mysql-deploy/prepareMysql2.sh
+++ terraform2/mvp-mysql-deploy/prepareMysql2.sh
@@ -14,44 +14,63 @@ echo ' *** Start MySQL service (enabled by default):  *** '
 systemctl start mysqld
 systemctl status mysqld
 
+echo ' *** Install expect *** '
+yum install expect -y
+
 echo ' *** Read in initial password of mysql *** '
 INITIALPASS=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $11}')
 echo $INITIALPASS
 
 echo ' *** Create helper script *** '
-cat <<EOF > /init_mysql.sh
-#!/bin/bash
-(
-  sleep 2
-  echo "\$1"    # Enter password for user root
-  sleep 2
-  echo "\$2"    # New password
-  sleep 2
-  echo "\$2"    # Re-enter new password
-  sleep 2
-  echo "y"     # Change the password for root ? (Press y|Y for Yes, any other key for No)
-  sleep 2
-  echo "\$2"    # New password
-  sleep 2
-  echo "\$2"    # Re-enter new password
-  sleep 2
-  echo "y"     # Do you wish to continue with the password provided? (Press y|Y for Yes, any other key for No)
-  sleep 2
-  echo "y"     # Remove anonymous users? (Press y|Y for Yes, any other key for No)
-  sleep 2
-  echo "y"     # Disallow root login remotely? (Press y|Y for Yes, any other key for No)
-  sleep 2
-  echo "y"     # Remove test database and access to it? (Press y|Y for Yes, any other key for No)
-  sleep 2
-  echo "y"     # Reload privilege tables now? (Press y|Y for Yes, any other key for No)
-  sleep 2
-) | mysql_secure_installation
+cat <<EOF > /init_mysql.exp
+#!/usr/bin/expect --
+
+set oldpass [lindex $argv 0]
+set newpass [lindex $argv 1]
+
+spawn /bin/mysql_secure_installation
+
+expect "Enter password for user root:"
+send "$oldpass\r"
+
+expect "New password:"
+send "$newpass\r"
+
+expect "Re-enter new password:"
+send "$newpass\r"
+
+expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "New password:"
+send "$newpass\r"
+
+expect "Re-enter new password:"
+send "$newpass\r"
+
+expect "Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
+send "y\r"
+
+puts "Ended expect script."
+
 EOF
-cat /init_mysql.sh
+cat /init_mysql.exp
 
 echo ' *** Run mysql_secure_installation *** '
-sudo chmod +x /init_mysql.sh
-sudo /init_mysql.sh $INITIALPASS $1
+#sudo chmod +x /init_mysql.exp
+#sudo /init_mysql.sh $INITIALPASS $1
 
 echo ' *** Enable remote access ???  *** '
 sed -i "s/bind-address.*/bind-address =*/g" /etc/my.cnf
