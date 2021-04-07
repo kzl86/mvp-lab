6ab9f6b9faeaf35310ed6bd0d6d1245b50fc3bfd
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-04-07 15:42:25 +0200

    .

--- terraform2/mvp-mysql-deploy/prepareWordpress.sh
+++ terraform2/mvp-mysql-deploy/prepareWordpress.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mysql -u root -p$1 << eof
+CREATE DATABASE wordpress;
 CREATE USER \`wp_admin\`@\`localhost\` IDENTIFIED BY '$2';
 CREATE USER \`wp_admin\`@\`%\` IDENTIFIED BY '$2';
 GRANT ALL ON wordpress.* TO \`wp_admin\`@\`localhost\`;
