982e56f12bf945a14c7b7d5de9c2a7ba2291c360
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-04-23 11:16:31 +0200

    ecr deployment terraform jenkins

---
+++ terraform2/mvp-ecr-deploy/ecr-mvp-us-east-1-aws.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+terraform {
+  backend "s3" {
+    bucket = "kiss-mvp"
+    key    = "mvp-lab-ecr"
+    region = "us-east-1"
+  }
+}
+
+variable "aws_access_key"     { type = string }
+variable "aws_secret_key"     { type = string }
+
+resource "aws_ecr_repository" "wordpress" {
+  name                 = "mvp-wordpress"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "tomcat" {
+  name                 = "mvp-tomcat"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
