0d6b78e4af1cbcf75c9c00dffcd8c2d64c081ccf
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-04-26 14:59:13 +0200

    wordpress shell script syntax error fix

--- mysql/wordpress/prepareWordpress.sh
+++ mysql/wordpress/prepareWordpress.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mysql -u root -p$1 << eof
 CREATE DATABASE wordpress;
-CREATE USER \`wp_admin\`@\`localhost\` IDENTIFIED BY '$2';
-CREATE USER \`wp_admin\`@\`%\` IDENTIFIED BY '$2';
-GRANT ALL ON wordpress.* TO \`wp_admin\`@\`localhost\`;
-GRANT ALL ON wordpress.* TO \`wp_admin\`@\`%\`;
+CREATE USER 'wp_admin'@'localhost' IDENTIFIED BY '$2';
+CREATE USER 'wp_admin'@'%' IDENTIFIED BY '$2';
+GRANT ALL ON wordpress.* TO 'wp_admin'@'localhost';
+GRANT ALL ON wordpress.* TO 'wp_admin'@'%';
 FLUSH PRIVILEGES;
 eof
