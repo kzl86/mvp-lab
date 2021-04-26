7f282a443d17f0a305fa7f301d3d7b4eb11d3416
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-04-26 17:24:25 +0200

    test

--- terraform2/prepareJenkinsNode2.sh
+++ terraform2/prepareJenkinsNode2.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-# Basic preparation for Jenkins node to be able to run Jenkins agent
-
-# This script is the initialization for mvp-proxy and mvp-mysql
-
-sudo yum update -y
-sudo yum -y install java-1.8.0-openjdk
-sudo yum -y install git
+yum update -y
+yum -y install java-1.8.0-openjdk
+yum -y install git
