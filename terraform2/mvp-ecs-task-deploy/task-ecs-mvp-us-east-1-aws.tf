2effa23080cfc7aee97078dbda6f58e69f2a7ad7
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-04-27 17:13:51 +0200

    JSON fixed syntax error

--- terraform2/mvp-ecs-task-deploy/task-ecs-mvp-us-east-1-aws.tf
+++ terraform2/mvp-ecs-task-deploy/task-ecs-mvp-us-east-1-aws.tf
@@ -59,7 +59,7 @@ resource "aws_ecs_task_definition" "mvp" {
                 "containerPort": 80,
                 "hostPort": 80
             }
-        ]
+        ],
         "mountPoints": [
           {
             "sourceVolume": "nfs",
