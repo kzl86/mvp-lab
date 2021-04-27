245db19191a8194006de834d3cb5e7648ef6eb29
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-04-27 17:40:10 +0200

    prepare ecs node with agent

--- terraform2/mvp-ecs-deploy/prepareECS.sh
+++ terraform2/mvp-ecs-deploy/prepareECS.sh
@@ -9,3 +9,13 @@ echo ECS_CLUSTER=mvp >> /etc/ecs/ecs.config
 yum update
 yum -y install java-1.8.0-openjdk
 yum -y install git
+
+# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-agent-install.html
+echo ' *** Disable the docker Amazon Linux extra repository. *** '
+sudo amazon-linux-extras disable docker
+
+echo ' *** Install and enable the ecs Amazon Linux extra repository. *** '
+sudo amazon-linux-extras install -y ecs; sudo systemctl enable --now ecs
+
+echo ' *** verify that the agent is running and see some information about your new container instance *** '
+curl -s http://localhost:51678/v1/metadata | python -mjson.tool
