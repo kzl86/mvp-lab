c7819ab3a6d7d3aa52cdaa95bf0904b92c50591e
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-04-28 11:44:48 +0200

    prepareECS.sh modified for Amazon EC2 AMI image

--- terraform2/mvp-ecs-deploy/prepareECS.sh
+++ terraform2/mvp-ecs-deploy/prepareECS.sh
@@ -4,11 +4,26 @@
 
 # This script is the initialization for ecs node
 
-echo ECS_CLUSTER=mvp >> /etc/ecs/ecs.config
-
+echo ' *** Prepare node as a jenkins slave *** '
 yum update
 yum -y install java-1.8.0-openjdk
 yum -y install git
+
+# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-agent-install.html
+echo ' *** Install the ecs-init package. For more information about ecs-init, see the source code on GitHub. *** '
+sudo amazon-linux-extras install ecs # this was not working: sudo yum install -y ecs-init (not found)
+
+echo ' *** Start the Docker daemon. *** '
+sudo service docker start
+
+echo ' *** Start the ecs-init upstart job. *** '
+# this was not working: sudo start ecs
+sudo systemctl enable ecs
+sudo systemctl start ecs
+
+echo ' *** Add cluster name to ecs.config *** '
+echo ECS_CLUSTER=mvp >> /etc/ecs/ecs.config
+
 
 # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-agent-install.html
 #echo ' *** Disable the docker Amazon Linux extra repository. *** '
