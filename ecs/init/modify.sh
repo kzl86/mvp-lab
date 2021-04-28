9331d28be4169725ffa9a3433e113605a98156d3
Kiss Zoltan Laszlo <laszlo.zoltan.kiss@gmail.com>
2021-04-28 14:45:40 +0200

    ecs initialization script fix

--- ecs/init/modify.sh
+++ ecs/init/modify.sh
@@ -7,12 +7,14 @@
 # $2 database password
 # $3 proxy IP address
 
+WORDPRESS_C_NAME=$(docker ps | grep wordpress | awk '{print $(NF)}')
+
 # Add IP address and password to the wordpress config file.
-docker exec -it wordpress sed -i "s/.*DB_PASSWORD.*/define('DB_PASSWORD', '$2');/g" /etc/wordpress/config.php
-docker exec -it wordpress sed -i "s/.*DB_HOST.*/define('DB_HOST', '$1');/g" /etc/wordpress/config.php
+docker exec -it $WORDPRESS_C_NAME sed -i "s/.*DB_PASSWORD.*/define('DB_PASSWORD', '$2');/g" /etc/wordpress/config.php
+docker exec -it $WORDPRESS_C_NAME sed -i "s/.*DB_HOST.*/define('DB_HOST', '$1');/g" /etc/wordpress/config.php
 
 # Create replicas from the config file using the IP address(es) of the host machine.
-for ip in $(hostname -I); do docker exec -it wordpress /bin/bash -c "cp /etc/wordpress/config.php /etc/wordpress/config-$ip.php"; done
+for ip in $(hostname -I); do docker exec -it $WORDPRESS_C_NAME /bin/bash -c "cp /etc/wordpress/config.php /etc/wordpress/config-$ip.php"; done
 
 # Create folder on host:
 mkdir /media/nfs
